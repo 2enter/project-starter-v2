c7772781e3c603c8c95165116a20f235092ee5a3
davilivob <david.iii.chiu@gmail.com>
2025-09-12 17:29:58 +0800

    dbmate init

---
+++ db/migrations/20250912072919_init.sql
@@ -0,0 +1,24 @@
+-- migrate:up
+CREATE TYPE locale AS ENUM('en', 'zh-tw');
+
+
+CREATE TABLE IF NOT EXISTS interaction (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMPTZ DEFAULT now(),
+  locale locale NOT NULL,
+  user_agent TEXT,
+  duration INTEGER
+);
+
+
+CREATE INDEX if NOT EXISTS interaction_created_at_idx ON interaction (created_at);
+
+
+-- migrate:down
+DROP TYPE IF EXISTS locale;
+
+
+DROP TABLE IF EXISTS interaction;
+
+
+DROP INDEX IF EXISTS interaction_created_at_idx;
