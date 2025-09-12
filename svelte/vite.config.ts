392d687a2173f9db304969418f0bcad6a236d47e
davilivob <david.iii.chiu@gmail.com>
2025-09-12 13:56:41 +0800

    [svelte] dev server config

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -2,6 +2,12 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+const { BACKEND_HOST, BACKEND_PORT } = process.env;
+const API_BASE_URL = `${BACKEND_HOST ?? 'localhost'}:${BACKEND_PORT ?? 3000}`;
+console.log('API base URL: ', API_BASE_URL);
 
 export default defineConfig({
 	plugins: [
@@ -11,5 +17,20 @@ export default defineConfig({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
 		})
-	]
+	],
+	server: {
+		host: '0.0.0.0',
+		proxy: {
+			'/api': {
+				target: `http://${API_BASE_URL}`,
+				changeOrigin: true,
+				secure: false
+			},
+			'/ws': {
+				target: `ws://${API_BASE_URL}`,
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
 });
