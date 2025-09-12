039188038b9ac864e84edda81a6b6fe15ab418ce
davilivob <david.iii.chiu@gmail.com>
2025-09-12 14:37:00 +0800

    [common] update lib.rs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,16 +1,13 @@
-mod api;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod api;
+pub mod model;
+pub mod utils;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        assert_eq!(true, true);
     }
 }
