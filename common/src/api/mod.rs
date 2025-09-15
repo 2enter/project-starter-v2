cee1beb6fd24f9e81be199c19b46ef1759a16ed1
davilivob <david.iii.chiu@gmail.com>
2025-09-15 15:51:44 +0800

    [common/api] remove unused struct

--- common/src/api/mod.rs
+++ common/src/api/mod.rs
@@ -3,10 +3,3 @@ pub mod response;
 
 pub use error::*;
 pub use response::*;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Debug, Deserialize)]
-pub struct IdOnly {
-    pub id: String,
-}
